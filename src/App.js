3f0caec3e8cbf6272b1e1c1ffd396abd1d6ba91d
Alexey Koryak <aleckoryak@gmail.com>
2025-02-26 12:50:11 -0500

    props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
+const Hello = (props) => <div>Hi there! {props.word}!</div>;
+const Greetings = ({firstName, lastName}) => <div>Hey you! {firstName} {lastName}!</div>;
+class Thanks extends React.Component {
+    render() {
+        return (
+            <div>Thank you! {this.props.firstName} {this.props.lastName}!
+
+                do not forget to add import React from 'react'; to work with classes
+            </div>
+        );
+    }
+}
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    <Hello word="React world"/>
+                    <Greetings firstName="Alec" lastName="Ko"/>
+                    <Thanks firstName="Alec" lastName="Ko"/>
+                </p>
+            </header>
+        </div>
+    );
 }
 
 export default App;

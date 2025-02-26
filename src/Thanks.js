197e1aa37891052b2a07c455096829e0ef6f4c70
Alexey Koryak <aleckoryak@gmail.com>
2025-02-26 13:30:39 -0500

    validations

--- src/Thanks.js
+++ src/Thanks.js
@@ -3,10 +3,7 @@ import React from "react";
 class Thanks extends React.Component {
     render() {
         return (
-            <div>Thank you! {this.props.firstName} {this.props.lastName}!
-
-                do not forget to add import React from 'react'; to work with classes
-            </div>
+            <div>Thank you! {this.props.firstName} {this.props.lastName}</div>
         );
     }
 }
--- src/forms/SimpleForm.js
+++ src/forms/SimpleForm.js
@@ -4,20 +4,49 @@ import Greetings from "../Greetings";
 class SimpleForm extends React.Component {
     state = {
         firstName: '',
-        lastName: ''
+        lastName: '',
+        errorMessage: ''
     }
-    onFirstNameChange = (event) => {
-        this.setState({firstName: event.target.value});
+
+    validateName = name => {
+        const regex = /[A-Za-z]{3,}/;
+        return !regex.test(name)
+            ? 'Name must be at least 3 characters long'
+            : '';
+    }
+    onFirstNameChange = event =>
+        this.setState({
+            firstName: event.target.value
+        });
+
+    onBlurFirstName = (event) => {
+        const errorMessage = this.validateName(this.state.firstName);
+        return this.state.errorMessage = errorMessage;
     }
     onLastNameChange = (event) => {
         this.setState({lastName: event.target.value});
     }
+
     render() {
+/*
+        const { errorMessage} = this.state;  {errorMessage}
+        equals to this.state.errorMessage
+        */
+        const { errorMessage, firstName } = this.state;
         return (
             <div>
-                <input type="text" name="firstName" onChange={this.onFirstNameChange} />
-                <input type="text" name="lastName" onChange={this.onLastNameChange} />
-                <Greetings firstName={this.state.firstName} lastName={this.state.lastName} />
+                <div>
+                    <label>First Name:
+                        <input type="text" name="firstName" onChange={this.onFirstNameChange}
+                               onBlur={this.onBlurFirstName}/>
+
+                        {this.state.errorMessage && <div>{errorMessage}</div>}
+                    </label>
+                    <label>Last Name:
+                        <input type="text" name="lastName" onChange={this.onLastNameChange}/>
+                    </label>
+                </div>
+                <Greetings firstName={this.state.firstName} lastName={this.state.lastName}/>
             </div>
         );
     }
